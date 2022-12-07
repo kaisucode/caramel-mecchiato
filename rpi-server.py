0e06e741e6cef357af3c63e7c9a2e0d3d767023a
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-12-07 17:15:01 -0500

    fixes from pi

--- rpi-server.py
+++ rpi-server.py
@@ -21,8 +21,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 settings = {
 	"mk6": {
-		"starting_pos": {15: 120, 14: 45, 0: 160}, # servo_pin, degrees
-		"ending_pos": {15: 30, 14: 0, 0: 100}, # servo_pin, degrees
+		"starting_pos": {15: 120, 14: 45, 0: 160, 1: 0, 2: 0}, # servo_pin, degrees
+		"ending_pos": {15: 30, 14: 0, 0: 100, 1: 60, 2: 60}, # servo_pin, degrees
 		"pins": {
 			"left_shoulder": 15,
 			"left_arm": 14,
@@ -35,6 +35,27 @@ settings = {
 }
 
 cur_mode = "mk6"
+
+class Sword: 
+	def __init__(self, servo_pin=3): 
+		self.servo = servo_pin
+
+	def stop_sword(self): 
+			test.move_servo_position(self.servo, 0)
+			print("sword stopped")
+
+	def extend_sword(self, reverse=False):
+
+		self.stop_sword()
+		if reverse: 
+			test.move_servo_position(self.servo, 0)
+			print("sword retracted")
+		else: 
+			test.move_servo_position(self.servo, 158)
+			print("sword extended")
+
+		time.sleep(1.5)
+		self.stop_sword()
 a_sword = Sword()
 
 def get_pin(name): 
@@ -45,7 +66,7 @@ def get_starting_pos(name):
 	return settings[cur_mode]["starting_pos"].get(pin, 0)
 def get_ending_pos(name): 
 	pin = get_pin(name)
-	return settings[cur_mode]["ending_pos"].get(pin, 30)
+	return settings[cur_mode]["ending_pos"].get(pin, 50)
 
 def circular_move_all(test_indices, move_range): 
     for val in test_indices: 
@@ -104,26 +125,6 @@ def initialize():
 	print("Initialized")
 
 
-class Sword: 
-	def __init__(self, servo_pin=3): 
-		self.servo = servo_pin
-
-	def stop_sword(self): 
-			test.move_servo_position(self.servo, 0)
-			print("sword stopped")
-
-	def extend_sword(self, reverse=False):
-
-		self.stop_sword()
-		if reverse: 
-			test.move_servo_position(self.servo, 0)
-			print("sword retracted")
-		else: 
-			test.move_servo_position(self.servo, 158)
-			print("sword extended")
-
-		time.sleep(1.5)
-		self.stop_sword()
 
 def shutdown(): 
 	test.restart()
@@ -164,10 +165,10 @@ def calibrate():
 
 def run_diagnostics(): 
 
-	for i in ["left_shoulder", "left_arm", "right_chest"]: 
+	for i in ["left_shoulder", "left_arm", "right_chest", "right_shoulder", "right_arm"]: 
 		test_high(i)
 	time.sleep(1)
-	for i in ["left_shoulder", "left_arm", "right_chest"]: 
+	for i in ["left_shoulder", "left_arm", "right_chest", "right_shoulder", "right_arm"]: 
 		test_low(i)
 
 @app.route('/voice_command', methods=["POST"])
@@ -177,22 +178,19 @@ def voice_command():
     print("action: ", action)
     print("part: ", part)
 
-	shutdown()
-
     if action == "initialize": 
         initialize()
     elif action == "calibrate": 
         calibrate()
-    elif action == "diagostics": 
+    elif action == "tests": 
         run_diagnostics()
+    elif action == "sword": 
+        a_sword.extend_sword()
     elif action == "raise" or "lift": 
         test_high(part)
     elif action == "lower": 
         tesT_low(part)
-    elif action == "sword": 
-        a_sword.extend_sword()
-
-	shutdown()
+    shutdown()
 
     #  socketio.emit('pi_do', { 'message': message })
     return jsonify({"status": "success"}), 200
@@ -217,8 +215,11 @@ def main():
 	return
 
 
+#host = "169.254.96.19"
+host = "172.18.132.80"
 if __name__ == "__main__":
-    socketio.run(app, host="169.254.96.19", port=PORT, debug=True)
+    print("starting server")
+    socketio.run(app, host=host, port=PORT, debug=True)
 
 
 
