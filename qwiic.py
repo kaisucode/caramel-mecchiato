9f6b8ef987c260528e39a6ae52b48bdb584e6124
Kevin Hsu <kevin.hsukaihao@gmail.com>
2022-12-07 02:30:17 -0500

    mark 6 wip

--- qwiic.py
+++ qwiic.py
@@ -4,7 +4,6 @@ import pi_servo_hat
 import time
 
 test = pi_servo_hat.PiServoHat()
-
 test.restart()
 
 
@@ -49,67 +48,104 @@ def test2():
 test_indices = [0, 1, 15]
 move_range = 35
 
-def smooth_move(servo, angle, reverse=False): 
+def smooth_move(servo, end, start=0): 
 
-    start, end, order = 0, angle, 1
-    if reverse: 
-        start, end, order = angle, 0, -1
+	direction = 1 if (end >= start) else -1
+	start, end, order = start, end, direction
 
-    for i in range(start, end, order): 
-        test.move_servo_position(servo, i)
-        time.sleep(0.005)
+	for i in range(start, end, order): 
+		test.move_servo_position(servo, i)
+		time.sleep(0.005)
 
 
-def rotation_test(servo): 
+def rotation_test(servo, smooth=True): 
 
-	test.move_servo_position(servo, 0)
+	#test.move_servo_position(servo, 0)
+	smooth_move(servo, 0)
 	print("moved to 0")
 	time.sleep(1)
 
-	test.move_servo_position(servo, 90)
+	#test.move_servo_position(servo, 90)
+	smooth_move(servo, 90)
 	print("moved to 90")
 	time.sleep(1)
 
-	test.move_servo_position(servo, 0)
+	#test.move_servo_position(servo, 0)
+	smooth_move(servo, 90, True)
 	print("moved to 0")
 	time.sleep(1)
 
 
 def initialize(): 
+	print("Initializing...")
 	for i in range(0, 16): 
 		test.move_servo_position(0, 0)
+	print("Initialized")
 
 
-def stop_sword(servo=3): 
-		test.move_servo_position(servo, 0)
-		print("sword stopped")
+class Sword: 
+	def __init__(self, servo_pin=3): 
+		self.servo = servo_pin
 
-def extend_sword(servo=3, reverse=False):
+	def stop_sword(self): 
+			test.move_servo_position(self.servo, 0)
+			print("sword stopped")
 
-	if reverse: 
-		test.move_servo_position(servo, 0)
-		print("sword retracted")
-	else: 
-		test.move_servo_position(servo, 158)
-		print("sword extended")
+	def extend_sword(self, reverse=False):
+
+		self.stop_sword()
+		if reverse: 
+			test.move_servo_position(self.servo, 0)
+			print("sword retracted")
+		else: 
+			test.move_servo_position(self.servo, 158)
+			print("sword extended")
+
+		time.sleep(1.5)
+		self.stop_sword()
+
+def shutdown(): 
+	test.restart()
+
+
+def left_shoulder_demo(): 
+	print("begin left shoulder demo")
+	smooth_move(15, start=0, end=50)
+
+	time.sleep(2)
+
+	smooth_move(15, start=30, end=0)
+	#rotation_test(15)
+	#test.move_servo_position(3, 0)
+	#smooth_move(3, 0)
+
+
+def run_tests(): 
+
+
+	left_shoulder_demo()
+
+	# sword demo
+	#a_sword.extend_sword()
+	#time.sleep(1)
+	#a_sword.extend_sword(reverse=True)
+
+
+
 	
+
 
 def main():
 	#circular_move_all(test_indices, move_range)
 	#smooth_move(15, 70)
 	#smooth_move(15, 70, reverse=True)
 
-	initialize()
-	print("initialized")
+	#initialize()
 
-	extend_sword()
-	time.sleep(1)
-	extend_sword(reverse=True)
+	run_tests()
+	a_sword = Sword()
 
-
-	#rotation_test(3)
-	#test.move_servo_position(3, 0)
-	#smooth_move(3, 0)
+	shutdown()
 	return
 
 	smooth_move(0, 0)
